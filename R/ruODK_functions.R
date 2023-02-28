96d3c121735216295c3a23b02a984a487ec2f3d0
Ernest Guevarra <ernest@guevarra.io>
2023-02-28 12:40:01 +0000

    fix #4

--- R/ruODK_functions.R
+++ R/ruODK_functions.R
@@ -4,7 +4,11 @@
 #' Get choices for a specified variable in an ODK form
 #'
 #' @param form_schema A form schema created by `ruODK::form_scheme_ext()`
-#' @param var_name Specific variable name to get choices of
+#' @param var_name Specific variable name in ODK form to get choices of
+#' @param choice_name Specific variable name in `form_schema` that holds
+#'   the information on choices. Forms with multiple languages will have a
+#'   `form_schema` with separate columns for choices for each language.
+#'   Default is NULL which assumes that only one language is used in the form.
 #'
 #' @return A tibble with `var_name`, `question`, `values`, and `labels`
 #'
@@ -22,20 +26,34 @@
 ################################################################################
 
 get_choices_ruodk <- function(form_schema,
-                              var_name = NULL) {
+                              var_name = NULL,
+                              choice_name = NULL) {
   if (!is.null(var_name)) {
     form_schema <- form_schema %>%
       dplyr::filter(.data$ruodk_name %in% var_name)
   }
 
-  suppressMessages(
-    choices <- form_schema %>%
-      dplyr::mutate(choices = lapply(.data$choices, dplyr::bind_cols)) %>%
-      tidyr::unnest(.data$choices) %>%
-      dplyr::filter(!is.na(.data$values)) %>%
-      dplyr::select("ruodk_name", "label", "values", "labels") %>%
-      dplyr::rename(var_name = .data$ruodk_name, question = .data$label)
-  )
+  if (!is.null(choice_name)) {
+    suppressMessages(
+      choices <- form_schema %>%
+        dplyr::mutate(
+          choices = lapply(.data[[choice_name]], dplyr::bind_cols)
+        ) %>%
+        tidyr::unnest(.data$choices) %>%
+        dplyr::filter(!is.na(.data$values)) %>%
+        dplyr::select("ruodk_name", "label", "values", "labels") %>%
+        dplyr::rename(var_name = .data$ruodk_name, question = .data$label)
+    )
+  } else {
+    suppressMessages(
+      choices <- form_schema %>%
+        dplyr::mutate(choices = lapply(.data$choices, dplyr::bind_cols)) %>%
+        tidyr::unnest(.data$choices) %>%
+        dplyr::filter(!is.na(.data$values)) %>%
+        dplyr::select("ruodk_name", "label", "values", "labels") %>%
+        dplyr::rename(var_name = .data$ruodk_name, question = .data$label)
+    )
+  }
 
   choices
 }
@@ -48,7 +66,11 @@ get_choices_ruodk <- function(form_schema,
 #'
 #' @param form_schema A form schema created by `ruODK::form_scheme_ext()`
 #' @param form_data A tibble of the data retrieved from ODK Central using ruODK
-#' @param var_name Specific variable name to get choices of
+#' @param var_name Specific variable name in ODK form to get choices of
+#' @param choice_name Specific variable name in `form_schema` that holds
+#'   the information on choices. Forms with multiple languages will have a
+#'   `form_schema` with separate columns for choices for each language.
+#'   Default is NULL which assumes that only one language is used in the form.
 #' @param other_var_name Variable name in `form_data` for other response in
 #'   `var_name`
 #'
@@ -73,9 +95,10 @@ get_choices_ruodk <- function(form_schema,
 ################################################################################
 
 match_other_to_choices <- function(form_schema, form_data,
-                                   var_name, other_var_name) {
+                                   var_name, choice_name = NULL,
+                                   other_var_name) {
   choices <- get_choices_ruodk(
-    form_schema = form_schema, var_name = var_name
+    form_schema = form_schema, var_name = var_name, choice_name = choice_name
   ) %>%
     dplyr::pull(.data$values) %>%
     tolower()
